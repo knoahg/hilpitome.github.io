d9d67c4cee6a29e8444b9e1d90430655bc742d48
hilpitome <hilpitome@gmail.com>
2015-09-26 19:00:41 +0300

    made custom.js function

---
+++ custom.js
@@ -0,0 +1,20 @@
+
+    $(function(){
+        $(".element").typed({
+            strings: ["Ruby on Rails.", "Java.", "android", "javascript", " and User Experience(UX)"],
+            typeSpeed: 0.5
+        });
+    });
+
+
+// <script type="text/javascript">
+// window.onload=timeout;
+// function timeout(){
+// window.setTimeout("redirect()",10000)}
+//
+// function redirect(){
+// window.location="home.html"
+// return}
+//
+//
+// </script>
