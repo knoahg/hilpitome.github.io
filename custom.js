964963e7c43c0244b0b1a6da86638d9b0189c644
hilpitome <hilpitome@gmail.com>
2015-09-30 03:42:47 +0300

    changed index page typing

--- custom.js
+++ custom.js
@@ -1,20 +1,17 @@
 
     $(function(){
         $(".element").typed({
-            strings: ["Ruby on Rails.", "Java.", "android", "javascript", " and User Experience(UX)"],
+            strings: ["Hi", "I am Hilary Egesa!!", "I am skilled in:","Ruby on Rails.", "Java.", "android", "javascript", " and User Experience(UX)"],
             typeSpeed: 0.5
         });
     });
 
 
-// <script type="text/javascript">
-// window.onload=timeout;
-// function timeout(){
-// window.setTimeout("redirect()",10000)}
-//
-// function redirect(){
-// window.location="home.html"
-// return}
-//
-//
-// </script>
+
+window.onload=timeout;
+function timeout(){
+window.setTimeout("redirect()",15000)}
+
+function redirect(){
+window.location="home.html"
+return}
