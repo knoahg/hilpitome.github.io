84a5df3ea5072fd9098566579cd718c029e74713
hilpitome <hilpitome@gmail.com>
2015-10-14 00:48:29 +0300

    added hero-slider from codyhouse

--- custom.js
+++ custom.js
@@ -8,5 +8,21 @@
       opacity: .5, // Opacity of modal background
       in_duration: 300, // Transition in duration
       out_duration: 200, // Transition out duration
-      
+
+    });
+    $('.cd-slider-nav li').on('click', function(event){
+    	event.preventDefault();
+    	var selectedItem = $(this);
+    	if(!selectedItem.hasClass('selected')) {
+    		// if it's not already selected
+    		var selectedPosition = selectedItem.index(),
+    			activePosition = $('.cd-hero-slider .selected').index();
+    		if( activePosition < selectedPosition) {
+    			nextSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
+    		} else {
+    			prevSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
+    		}
+
+    		updateNavigationMarker(selectedPosition+1);
+    	}
     });
