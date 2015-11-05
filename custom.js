d7541e05ead6e5569fc44801ecf0d87b78f82474
hilpitome <hilpitome@gmail.com>
2015-11-05 18:29:28 +0300

    refactored the whole site

--- custom.js
+++ custom.js
@@ -10,20 +10,6 @@
       out_duration: 200, // Transition out duration
 
     });
-    
-    $('.cd-slider-nav li').on('click', function(event){
-    	event.preventDefault();
-    	var selectedItem = $(this);
-    	if(!selectedItem.hasClass('selected')) {
-    		// if it's not already selected
-    		var selectedPosition = selectedItem.index(),
-    			activePosition = $('.cd-hero-slider .selected').index();
-    		if( activePosition < selectedPosition) {
-    			nextSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-    		} else {
-    			prevSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-    		}
 
-    		updateNavigationMarker(selectedPosition+1);
-    	}
-    });
+    
+    $(".button-collapse").sideNav()
