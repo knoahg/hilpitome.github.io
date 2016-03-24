f375eddb2ab036019ad31f5dc2fca87e56293271
hilary <hilpitome@gmail.com>
2016-03-24 12:40:01 +0300

    added carousel

--- custom.js
+++ custom.js
@@ -5,11 +5,16 @@
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
    $('.modal-trigger').leanModal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
+      opacity: 0.5, // Opacity of modal background
       in_duration: 300, // Transition in duration
       out_duration: 200, // Transition out duration
 
     });
 
-    
+
     $(".button-collapse").sideNav()
+    $('.carousel').carousel({
+        time_constant: 1000,
+        shift: 2000
+
+    });
